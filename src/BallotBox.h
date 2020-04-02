3f9502cfb1c038699ad86460da870d510ab0808d
Christopher Hanson <hans4837@umn.edu>
2020-04-01 23:54:28 -0500

    start of ballotbox implementation; added example election file for testing purposes

---
+++ src/BallotBox.h
@@ -0,0 +1,15 @@
+#ifndef SRC_BALLOTBOX_H
+#define SRC_BALLOXBOX_H
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+class BallotBox {
+  public:
+    BallotBox();
+};
+
+
+
+#endif //SRC_BALLOTBOX_H
